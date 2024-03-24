a84727dc228c91dcad08ff85f4f5cbcf293dbcf8
sammi <sshih5678@gmail.com>
2024-03-23 23:18:18 -0700

    redirect to sign in from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@
 import "./calendar.css";
 
 import { Lato } from "next/font/google";
-import { useContext } from "react";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect } from "react";
 import { UserContext } from "@/context/UserContext";
 import Cal from "./Calendar";
 
@@ -11,11 +12,18 @@ const latoBold = Lato({ subsets: ["latin"], weight: "900" });
 
 export default function Home() {
   const { user } = useContext(UserContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      router.replace("/auth/signin");
+    }
+  }, [router, user]);
 
   return (
-    <div className="flex flex-col items-start justify-center text-highlight p-11">
-      {user ? (
-        <div>
+    <div>
+      {user && (
+        <div className="flex flex-col items-start justify-center text-highlight p-11">
           <div className={`${latoBold.className} h-32`}>
             <h1 className="text-2xl">Hello,</h1>
             <h1 className="text-4xl">{user.name.split(" ")[0]}.</h1>
@@ -25,8 +33,6 @@ export default function Home() {
             <h1 className="text-3xl">Overview</h1>
           </div>
         </div>
-      ) : (
-        <></>
       )}
     </div>
   );
